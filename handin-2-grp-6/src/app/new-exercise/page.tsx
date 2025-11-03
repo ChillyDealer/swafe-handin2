13ef50f8a97ec005ee0278bcd624ccf0b76e614e
MortenT <mtlnielsen@gmail.com>
2025-11-03 16:33:52 +0100

    centered a div

--- handin-2-grp-6/src/app/new-exercise/page.tsx
+++ handin-2-grp-6/src/app/new-exercise/page.tsx
@@ -3,15 +3,13 @@ import { PageComponent } from "../_components/page-component";
 
 export default function NewExercise() {
   return (
-    <PageComponent>
-      <div className="flex flex-col items-center justify-center">
-        <Heading className="p-4" size="8">
-          Create a New Exercise
-        </Heading>
-        <TextField.Root className="w-1/3">
-          <TextField.Slot>Exercise name</TextField.Slot>
-        </TextField.Root>
-      </div>
+    <PageComponent className="flex flex-col items-center justify-center">
+      <Heading className="p-4" size="8">
+        Create a New Exercise
+      </Heading>
+      <TextField.Root className="w-1/3">
+        <TextField.Slot>Exercise name</TextField.Slot>
+      </TextField.Root>
     </PageComponent>
   );
 }
