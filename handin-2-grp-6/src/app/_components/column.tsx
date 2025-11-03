f194007c5234a7d65d0e2c969bbafcebb318aed3
OliverEC <oliverec04@gmail.com>
2025-11-03 16:25:11 +0100

    added row, col, page component, and other

---
+++ handin-2-grp-6/src/app/_components/column.tsx
@@ -0,0 +1,11 @@
+﻿import React, {type FC, type HTMLProps} from 'react';
+
+export const Column: FC<HTMLProps<HTMLDivElement>> = (props) => {
+    const {children, className} = props;
+
+    return (
+        <div {...props} className={'flex flex-col ' + className}>
+            {children}
+        </div>
+    );
+};
---
+++ handin-2-grp-6/src/app/_components/page-component.tsx
@@ -0,0 +1,12 @@
+﻿import React, {type FC, type HTMLProps} from 'react';
+import {Row} from "@/app/_components/row";
+
+export const PageComponent: FC<HTMLProps<HTMLDivElement>> = (props) => {
+    const {children, className} = props;
+
+    return (
+        <Row {...props} className={"h-full w-full" + className}>
+            {children}
+        </Row>
+    );
+};
