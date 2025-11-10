3ea0e505fb8ce2faba66b6b98526cb44f06a8dad
MortenT <mtlnielsen@gmail.com>
2025-11-10 12:01:32 +0100

    exercise form

--- handin-2-grp-6/src/app/new-exercise/page.tsx
+++ handin-2-grp-6/src/app/new-exercise/page.tsx
@@ -1,15 +1,45 @@
-import { Flex, Heading, TextField } from "@radix-ui/themes";
+import { Flex, Heading, RadioGroup, TextField } from "@radix-ui/themes";
 import { PageComponent } from "../_components/page-component";
+import { Column } from "../_components/column";
+import { Row } from "../_components/row";
 
 export default function NewExercise() {
+  let usingSets: boolean = true;
   return (
     <PageComponent className="flex flex-col items-center justify-center">
-      <Heading className="p-4" size="8">
-        Create a New Exercise
-      </Heading>
-      <TextField.Root className="w-1/3">
-        <TextField.Slot>Exercise name</TextField.Slot>
-      </TextField.Root>
+      <Column>
+        <Heading className="p-4 text-center" size="8">
+          Create a New Exercise
+        </Heading>
+        <TextField.Root className="w-1/3 mx-auto">
+          <TextField.Slot>Exercise name</TextField.Slot>
+        </TextField.Root>
+        <Row className="items-center">
+          {/* 3 text fields, hvor den første er større end de andre 2 */}
+          <TextField.Root className="flex-2">
+            <TextField.Slot>Description</TextField.Slot>
+          </TextField.Root>
+          <TextField.Root className="flex-1">
+            <TextField.Slot>Reps</TextField.Slot>
+          </TextField.Root>
+          <TextField.Root className="flex-1">
+            <TextField.Slot>
+              {usingSets ? "Sets" : "Duration (mins)"}
+            </TextField.Slot>
+          </TextField.Root>
+          {/* Radio group med time og sets som muligheder */}
+          <Column>
+            <RadioGroup.Root>
+              <RadioGroup.Item value="sets" id="sets">
+                Sets
+              </RadioGroup.Item>
+              <RadioGroup.Item value="time" id="time">
+                Time
+              </RadioGroup.Item>
+            </RadioGroup.Root>
+          </Column>
+        </Row>
+      </Column>
     </PageComponent>
   );
 }
