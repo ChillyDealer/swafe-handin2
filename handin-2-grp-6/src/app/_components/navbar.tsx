bcae15d81067bec84031ba63d6f9ab4d7dd51851
OliverEC <oliverec04@gmail.com>
2025-11-17 13:43:11 +0100

    client side

--- handin-2-grp-6/src/app/_components/navbar.tsx
+++ handin-2-grp-6/src/app/_components/navbar.tsx
@@ -27,32 +27,34 @@ export const Navbar = () => {
         <Column>
             <Row className="h-14 items-center justify-between p-4 bg-[#53659a]">
                 <Link href="/" className="flex items-center gap-2">
-                    <img src="/favicon.ico" alt="icon" className="w-6 h-6" />
+                    <img src="/favicon.ico" alt="icon" className="w-6 h-6"/>
                     <p className="text-white font-bold text-lg cursor-pointer">Sigma Fitness</p>
                 </Link>
-                {isLoggedIn ? (
-                    <>
-                        <Link href="/users">
+                <Row>
+                    {isLoggedIn ? (
+                        <>
+                            <Link href="/users">
+                                <button
+                                    className="text-white font-bold text-lg drop-shadow-md hover:drop-shadow-lg transition-all cursor-pointer mr-4">
+                                    Users
+                                </button>
+                            </Link>
+                            <button
+                                onClick={handleLogout}
+                                className="text-white font-bold text-lg drop-shadow-md hover:drop-shadow-lg transition-all cursor-pointer mr-4"
+                            >
+                                Logout
+                            </button>
+                        </>
+                    ) : (
+                        <Link href="/login">
                             <button
                                 className="text-white font-bold text-lg drop-shadow-md hover:drop-shadow-lg transition-all cursor-pointer mr-4">
-                                Users
+                                Login
                             </button>
                         </Link>
-                        <button
-                            onClick={handleLogout}
-                            className="text-white font-bold text-lg drop-shadow-md hover:drop-shadow-lg transition-all cursor-pointer mr-4"
-                        >
-                            Logout
-                        </button>
-                    </>
-                ) : (
-                    <Link href="/login">
-                        <button
-                            className="text-white font-bold text-lg drop-shadow-md hover:drop-shadow-lg transition-all cursor-pointer mr-4">
-                            Login
-                        </button>
-                    </Link>
-                )}
+                    )}
+                </Row>
             </Row>
             <Separator size="4"/>
         </Column>
