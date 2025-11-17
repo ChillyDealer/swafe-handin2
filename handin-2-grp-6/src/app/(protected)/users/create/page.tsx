08e8fb5d173ee6c4a7584ce72f3013e3f60a8f86
OliverEC <oliverec04@gmail.com>
2025-11-17 14:24:37 +0100

    slut

--- handin-2-grp-6/src/app/(protected)/users/create/page.tsx
+++ handin-2-grp-6/src/app/(protected)/users/create/page.tsx
@@ -1,9 +1,12 @@
-﻿import {Flex} from "@radix-ui/themes";
+﻿import { Flex } from '@radix-ui/themes';
+import { InputField } from '@/app/_components/input/input-field';
 
 export default function CreateUserPage() {
-    return (
-        <Flex direction="column" className="p-8">
-            new
-        </Flex>
-    );
+  return (
+    <Flex direction='column' className='p-8'>
+      <form>
+        <InputField />
+      </form>
+    </Flex>
+  );
 }
---
+++ handin-2-grp-6/src/app/_components/input/input-field.tsx
@@ -0,0 +1,33 @@
+﻿import { TextField } from '@radix-ui/themes';
+import { FileText } from 'lucide-react';
+import { FC, ReactNode } from 'react';
+
+interface Props {
+  slot: ReactNode;
+}
+
+export const InputField: FC<Props> = (props) => {
+  const { slot } = props;
+
+  return (
+    <TextField.Root placeholder='Search the docs…'>
+      <TextField.Slot>{slot}</TextField.Slot>
+    </TextField.Root>
+  );
+
+  // return (
+  //   <div className='flex flex-col gap-2'>
+  //     <label className='text-white text-xs'>Description</label>
+  //     <div className='flex items-start bg-[#3a3a3a] rounded-md px-3 py-2 gap-2'>
+  //       <FileText className='text-gray-400 mt-1' size={18} />
+  //       <textarea
+  //         placeholder='Description'
+  //         value={description}
+  //         onChange={(e) => setDescription(e.target.value)}
+  //         className='bg-transparent text-white text-sm outline-none flex-1 resize-none min-h-20'
+  //         required
+  //       />
+  //     </div>
+  //   </div>
+  // );
+};
