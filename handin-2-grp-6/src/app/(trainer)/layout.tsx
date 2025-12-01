3a8db8e6eea4a5c5620e437017e2f3ffae74aa2d
MortenT <mtlnielsen@gmail.com>
2025-12-01 13:47:42 +0100

    protected workout programs route

---
+++ handin-2-grp-6/src/app/(trainer)/layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AuthService } from '@/app/services/auth.service';
+import { Column } from '@/app/_components/column';
+import { Navbar } from '@/app/_components/navbar';
+import { Text } from '@radix-ui/themes';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default async function ProtectedLayout(props: Props) {
+  const { children } = props;
+
+  const role = await AuthService.getRole();
+
+  if (role === 'PersonalTrainer') {
+    return (
+      <Column>
+        <Navbar />
+        {children}
+      </Column>
+    );
+  }
+
+  return (
+    <Column className='items-center p-8'>
+      <Text size='6'>Her må du sgu ikke være lille knejt</Text>
+    </Column>
+  );
+}
--- handin-2-grp-6/src/app/(trainer)/programs/page.tsx
+++ handin-2-grp-6/src/app/(trainer)/programs/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { PageComponent } from '../_components/page-component';
-import { getTrainerWorkoutPrograms } from '../_data/exercises-api';
-import { WorkoutProgram } from '../myworkouts/types';
-import { Navbar } from '../_components/navbar';
-import { Column } from '../_components/column';
-import { WorkoutCard } from '../myworkouts/components/workout-card';
+import { PageComponent } from '../../_components/page-component';
+import { getTrainerWorkoutPrograms } from '../../_data/exercises-api';
+import { WorkoutProgram } from '../../myworkouts/types';
+import { Navbar } from '../../_components/navbar';
+import { Column } from '../../_components/column';
+import { WorkoutCard } from '../../myworkouts/components/workout-card';
 
 export default function ProgramsPage() {
   const [programs, setPrograms] = useState<WorkoutProgram[]>([]);
@@ -23,7 +23,7 @@ export default function ProgramsPage() {
 
   return (
     <PageComponent>
-      <Navbar />
+      {/* <Navbar /> */}
       <Column className='min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex flex-col items-center justify-center w-full'>
         <h1 className='text-white text-2xl font-bold text-center mb-6'>
           My programs:{' '}
