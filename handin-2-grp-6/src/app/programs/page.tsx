62adace1c76418658f9eecc01be8d4fd605d8bc2
MortenT <mtlnielsen@gmail.com>
2025-12-01 13:27:53 +0100

    align stylign baggrund

--- handin-2-grp-6/src/app/programs/page.tsx
+++ handin-2-grp-6/src/app/programs/page.tsx
@@ -24,8 +24,8 @@ export default function ProgramsPage() {
   return (
     <PageComponent>
       <Navbar />
-      <Column className='flex flex-col items-center justify-center w-full'>
-        <h1 className='text-black text-2xl font-bold text-center mb-6'>
+      <Column className='min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex flex-col items-center justify-center w-full'>
+        <h1 className='text-white text-2xl font-bold text-center mb-6'>
           My programs:{' '}
         </h1>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full'>
